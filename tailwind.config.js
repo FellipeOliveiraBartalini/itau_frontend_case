80bec2f773bd398ecb13adaf439446859de82e9a
FellipeOliveiraBartalini <fellipe.oliveira.work@gmail.com>
2024-09-18 10:59:34 -0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,ts}'],
+    theme: {
+        // TODO: fix the custom colors
+        // colors: {
+        //   'orange-itau': '#FF6200',
+        // }
+        fontFamily: {
+            sans: ['Nunito', 'sans-serif'],
+        },
+    },
+    plugins: [],
+};
